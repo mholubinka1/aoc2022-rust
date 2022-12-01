2c8acd98ed7cea8fa4d8a7238d63e19a6bde04a2
Matt Holubinka <meholubinka@gmail.com>
2022-12-01 13:30:22 +0000

    complete day 1 puzzle

--- day-1/src/main.rs
+++ day-1/src/main.rs
@@ -1,12 +1,46 @@
 const TODAY_SAMPLE: &str = "
 ";
 
-fn load_input() -> String {
-    std::fs::read_to_string("../input").unwrap()
+struct Elf {
+    number: i64,
+    calories: i64,
 }
 
+impl Elf {
+    pub fn new(number: i64, calories: i64) -> Self {
+        Elf {
+            number,
+            calories
+        }
+    }
+}
+
+fn load_elves() -> Vec<Elf> {
+    let input: String = std::fs::read_to_string("../input").unwrap();
+    let mut elves: Vec<Elf> = Vec::new();
+    let mut tot_cals: i64 = 0;
+    let mut elf_count: i64 = 1;
+    for line in input.lines() {
+        if line.is_empty() {
+            elves.push(Elf::new(elf_count, tot_cals));
+            tot_cals = 0;
+            elf_count += 1;
+        } else {
+            tot_cals += line.parse::<i64>().unwrap()
+        }
+    }
+    elves.push(Elf::new(elf_count, tot_cals));
+    elves
+}
+
+
 fn main() {
-    let input = load_input();
-    println!("{}", TODAY_SAMPLE);
-    println!("{}", input);
+    let mut _elves = load_elves();
+    _elves.sort_by_key(|x| x.calories);
+    _elves.reverse();
+    //solution to part 1
+    println!("Elf:{} | Carrying {} calories.",_elves[0].number, _elves[0].calories);
+    //solution to part 2
+    let sum_top_3: i64 = _elves[..3].iter().map(|x| x.calories).sum();
+    println!("Top 3 elves are carrying {} calories.", sum_top_3);
 }
--- input_fetch.sh
+++ input_fetch.sh
@@ -5,8 +5,10 @@ year=2022
 day=$(date +%-d)
 dir=day-$day
 
+## creates a new rust project
 cargo new $dir
 curl -b $(cat ~/.cookie) "https://adventofcode.com/$year/day/$day/input" \
     > $dir/input
 
+## creates a new main based on boilerplate code - a simple function to load the input data into a string and a main
 cp template.rs $dir/src/main.rs
