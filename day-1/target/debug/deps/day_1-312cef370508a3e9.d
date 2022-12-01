31d6afca427f259dc2465ad458e13d3eafcca7aa
Matt Holubinka <meholubinka@gmail.com>
2022-12-01 12:35:19 +0000

    remove unused files

--- day-1/target/debug/deps/day_1-312cef370508a3e9.d
+++
@@ -1,5 +0,0 @@
-c:\Users\mehol\git\aoc2022-rust\day-1\target\debug\deps\day_1-312cef370508a3e9.rmeta: src\main.rs
-
-c:\Users\mehol\git\aoc2022-rust\day-1\target\debug\deps\day_1-312cef370508a3e9.d: src\main.rs
-
-src\main.rs:
--- day-1/target/debug/deps/day_1-4e61f8e1a06e7cba.d
+++
@@ -1,5 +0,0 @@
-c:\Users\mehol\git\aoc2022-rust\day-1\target\debug\deps\day_1-4e61f8e1a06e7cba.rmeta: src\main.rs
-
-c:\Users\mehol\git\aoc2022-rust\day-1\target\debug\deps\day_1-4e61f8e1a06e7cba.d: src\main.rs
-
-src\main.rs:
