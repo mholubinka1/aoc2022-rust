14d17b3c6746e3f51b99bd7d1d5afcb207b18b1e
Matt Holubinka <meholubinka@gmail.com>
2022-12-07 10:02:44 +0000

    start day 7

---
+++ day-7/src/main.rs
@@ -0,0 +1,13 @@
+const SAMPLE: &str = "
+";
+
+fn load_input() -> String {
+    std::fs::read_to_string("../input").unwrap()
+}
+
+fn main() {
+    //let input = SAMPLE.to_string();
+    let input = load_input();
+    println!("{}", SAMPLE);
+    println!("{}", input);
+}
